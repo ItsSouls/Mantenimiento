1bb15d504d708882b526d166fe2a1de11fdf770c
ItsSouls <ItsSouls18@gmail.com>
2023-02-03 11:54:43 +0100

    First commit

---
+++ src/test/java/DCB/mantenimineto/FactorialTest.java
@@ -0,0 +1,56 @@
+package DCB.mantenimineto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+    Test cases
+    1. factorial 0 -> 1
+    2. factorial 1 -> 1
+    3. factorial 2 -> 2
+    3. factorial 3 -> 6
+ */
+
+class FactorialTest {
+    @Test
+    void factorialOfZeroIsOne(){
+        var factorial = new Factorial(); // es lo mismo que poner Factorial factorial = new Factorial();
+        int obtainedValue = factorial.compute(0);
+        int expectedValue = 1;
+
+        assertEquals(expectedValue,obtainedValue);
+    }
+    @Test
+    void factorialOfOneIsOne(){
+        var factorial = new Factorial(); // es lo mismo que poner Factorial factorial = new Factorial();
+        int obtainedValue = factorial.compute(1);
+        int expectedValue = 1;
+
+        assertEquals(expectedValue,obtainedValue);
+    }
+    @Test
+    void factorialOfTwoIsTwo(){
+        var factorial = new Factorial(); // es lo mismo que poner Factorial factorial = new Factorial();
+        int obtainedValue = factorial.compute(2);
+        int expectedValue = 2;
+
+        assertEquals(expectedValue,obtainedValue);
+    }
+    @Test
+    void factorialOfThreeIsSix(){
+        var factorial = new Factorial(); // es lo mismo que poner Factorial factorial = new Factorial();
+        int obtainedValue = factorial.compute(3);
+        int expectedValue = 6;
+
+        assertEquals(expectedValue,obtainedValue);
+    }
+    @Test
+    void factorialOf5Is120(){
+        var factorial = new Factorial(); // es lo mismo que poner Factorial factorial = new Factorial();
+        int obtainedValue = factorial.compute(5);
+        int expectedValue = 120;
+
+        assertEquals(expectedValue,obtainedValue);
+    }
+}
