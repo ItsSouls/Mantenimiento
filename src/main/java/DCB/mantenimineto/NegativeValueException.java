80ea5b78bf23e82d7f413d0cd8399e7cdb8ac267
ItsSouls <ItsSouls18@gmail.com>
2023-02-09 17:58:48 +0100

    actualizacion de repositorio

--- src/main/java/DCB/mantenimineto/NegativeValueException.java
+++
@@ -1,7 +0,0 @@
-package DCB.mantenimineto;
-
-public class NegativeValueException extends RuntimeException{
-    public NegativeValueException(String errormessage) {
-        super(errormessage);
-    }
-}
